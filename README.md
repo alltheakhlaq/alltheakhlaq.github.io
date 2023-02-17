da8fadaa41143204d0fc50e42635f9a29b7624b4
SaraAkhlaq <77059785+alltheakhlaq@users.noreply.github.com>
2023-02-17 15:44:00 +0100

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-<p> Hey! I'm Sara Akhlaq and this is my personal website that showcases my research and design experience.</p> 
-<p> I used React and Gatsby to make it. </p> 
-
--- src/assets/css/color.js
+++
@@ -1,9 +0,0 @@
-export const BACKGROUND = "#F7B2B7";
-
-export const BROWN = "#C2847A";
-
-export const DARK = "#243B4A";
-
-export const PINK = "#A2708A";
-
-export const GREEN = "#a4af69";
