8aeb874ba1581bfe86f13b48cb187dac7afe60eb
Sara <77059785+alltheakhlaq@users.noreply.github.com>
2024-09-04 10:55:27 +0200

    Updates

---
+++ component---src-pages-contact-js-2653ab369f3bcd54b161.js
@@ -0,0 +1,22 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[209],{5351:function(e,t,a){a.r(t),a.d(t,{Head:function(){return p}});var n=a(6540),l=a(9445),i=a(2568),r=a(4496),o=(a(8756),a(3651));const c=i.Ay.section`
+  /* background-color: ${o.Rt};
+  padding: 80px; */
+
+  & p {
+    background: ${o.BLACK};
+  }
+
+  @media only screen and (min-width: 701px) {
+    /* padding-left: 100px;
+    padding-right: 100px; */
+    padding-top: 30px;
+    margin-left: 100px;
+    margin-right: 100px;
+  }
+  @media only screen and (max-width: 700px) {
+    padding-top: 10px;
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+`;t.default=()=>n.createElement(l.A,null,n.createElement(c,null,n.createElement("h2",null,"I'm happy to talk more with you!"),n.createElement("hr",null),n.createElement("p",null,"I have a wide variety of interests including but not limited to academic research on application of intersectional feminist principles to different topic in digital humanities. I also offer design and development skills in webdesign, graphic design and front-end web development."),n.createElement("p",null,"If you feel we could work together or if you need any kind of support I can offer, please feel free to get in touch!"),n.createElement("b",null,n.createElement("p",null,"Email: saara.akhlaq@gmail.com")),n.createElement("b",null,n.createElement("p",null,"Twitter:"," ",n.createElement("a",{href:"https://twitter.com/alltheakhlaq"},"@alltheakhlaq"))),n.createElement("p",null,"Especially, if you are a woman of color, I am particularly keen to support you in whatever way I can so don't hesitate contacting me!")));const p=()=>n.createElement(r.k,{title:"Contact | Sara Akhlaq"})}}]);
+//# sourceMappingURL=component---src-pages-contact-js-2653ab369f3bcd54b161.js.map
