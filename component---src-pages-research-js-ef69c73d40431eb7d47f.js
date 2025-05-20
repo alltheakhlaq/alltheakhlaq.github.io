721aa8ed4df6903e9e66c1aa88b5f1364dc36ea2
Sara <77059785+alltheakhlaq@users.noreply.github.com>
2025-05-20 15:14:02 +0200

    Updates

---
+++ component---src-pages-research-js-ef69c73d40431eb7d47f.js
@@ -0,0 +1,121 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[82],{4792:function(e,t,n){n.d(t,{A:function(){return E}});var a=n(6540),l=n(2568),r=n(3651);const i=l.Ay.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`,o=l.Ay.div`
+  display: flex;
+  width: 100%;
+  position: relative;
+`,c=l.Ay.div`
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+`,s=l.Ay.div`
+  display: flex;
+  transition: all 250ms linear;
+  -ms-overflow-style: none; /* hide scrollbar in IE and Edge */
+  scrollbar-width: none; /* hide scrollbar in Firefox */
+  ::-webkit-scrollbar,
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  > * {
+    width: 100%;
+    flex-shrink: 0;
+    flex-grow: 1;
+  }
+`,u=l.AH`
+  position: absolute;
+  z-index: 1;
+  top: 50%;
+  transform: translateY(-50%);
+  border-radius: 24px;
+  background-color: white;
+  border: 1px solid #ddd;
+  @media only screen and (min-width: 701px) {
+    width: 48px;
+    height: 48px;
+  }
+  @media only screen and (max-width: 700px) {
+    width: 20px;
+    height: 20px;
+  }
+`,m=l.Ay.button`
+  ${u}
+  @media only screen and (min-width: 701px) {
+    left: 24px;
+  }
+  @media only screen and (max-width: 700px) {
+    left: -20px;
+  }
+`,h=l.Ay.button`
+  ${u}
+  @media only screen and (min-width: 701px) {
+    right: 24px;
+  }
+  @media only screen and (max-width: 700px) {
+    right: -20px;
+  }
+`;var d=(0,l.Ay)((e=>{const{children:t}=e,{0:n,1:l}=(0,a.useState)(0),{0:r,1:u}=(0,a.useState)(t.length);return(0,a.useEffect)((()=>{u(t.length)}),[t]),a.createElement(i,null,a.createElement(o,null,a.createElement(m,{onClick:()=>{n>0&&l((e=>e-1))}},"<"),a.createElement(c,null,a.createElement(s,{style:{transform:`translateX(-${100*n}%)`}},t.map((e=>a.createElement("div",null,a.createElement("div",{className:"slider"},e)))))),a.createElement(h,{onClick:()=>{n<r-1&&l((e=>e+1))}},">")))}))`
+  background-color: ${r.h4};
+`;const p=l.Ay.section`
+  background-color: ${r.Rt};
+  font-family: "IBM Plex Mono", monospace;
+
+  td {
+    color: ${r.nd};
+  }
+  th {
+    color: ${r.nd};
+  }
+
+  tr {
+    border: 1px solid black;
+  }
+
+  @media only screen and (min-width: 701px) {
+    padding: 15px;
+
+    table {
+      padding-left: 90px;
+      padding-right: 90px;
+    }
+
+    tr {
+      padding: 100px;
+      border: 1px solid black;
+    }
+
+    th {
+      width: 180px;
+    }
+  }
+
+  @media only screen and (max-width: 700px) {
+    padding: 10px;
+
+    tr {
+      padding: 10px;
+      border: 1px solid black;
+    }
+
+    th {
+      width: 50px;
+      padding-right: 5px;
+    }
+  }
+`;var E=()=>a.createElement(p,null,a.createElement("h2",null,"Recent activities"),a.createElement(d,null,a.createElement("table",null,a.createElement("tr",null,a.createElement("th",null,a.createElement("p",null,a.createElement("b",null,"01.02.2025 - 30.11.2025"))),a.createElement("td",null,a.createElement("p",null,"Awarded"," ",a.createElement("a",{href:"https://www.museumfuernaturkunde.berlin/en/museum/today/museum/themuseumslab"}," ","TheMusuemLab")," ","2025 fellowship. Theme of this year is 'Colonialism and Resistance'."))),a.createElement("tr",null,a.createElement("th",null,a.createElement("p",null,a.createElement("b",null,"03.03.2025"))),a.createElement("td",null,a.createElement("p",null,"New publication: Díez Díaz, V., Akhlaq, S., Kaiser, K. et al. (2025). Nat. Rev. Biodivers. 1, 145–146 (2025)."," ",a.createElement("a",{href:"https://www.nature.com/articles/s44358-025-00031-2"}," ","Digitization as a research methodology in colonial natural history collections.")," ","https://doi.org/10.1038/s44358-025-00031-2"))),a.createElement("tr",null,a.createElement("th",null,a.createElement("p",null,a.createElement("b",null,"22.12.2024/2025"))),a.createElement("td",null,a.createElement("p",null,"New Publication: Depraetere, M., Akhlaq, S., Díaz, V.D., Heumann, I., Schwarz, D. (2025)."," ",a.createElement("a",{href:"https://link.springer.com/chapter/10.1007/978-3-031-78590-0_8#citeas"},"Virtual Access to Fossil & Archival Material from the German Tendaguru Expedition (1909–1913): More Than 100 years of Data-Meta-paradata Management for Improved Standardisation."),"In: Ioannides, M., Baker, D., Agapiou, A., Siegkas, P. (eds) 3D Research Challenges in Cultural Heritage V. Lecture Notes in Computer Science, vol 15190. Springer, Cham. https://doi.org/10.1007/978-3-031-78590-0_8")))),a.createElement("table",null,a.createElement("tr",null,a.createElement("th",null,a.createElement("p",null,a.createElement("b",null," 01.03.2024 - Present "))),a.createElement("td",null,a.createElement("p",null,"Organising and facilitating series of internal workshops, as part of the project ",a.createElement("a",{href:"https://www.museumfuernaturkunde.berlin/en/science/virtual-access-fossil-and-archival-material-german-tendaguru-expedition-1909-1913"},"Virtual access to fossil and archival material from the German Tendaguru Expedition (1909-1913)"),", in order to discuss just representation of musuem data of colonial nature"))),a.createElement("tr",null,a.createElement("th",null,a.createElement("p",null,a.createElement("b",null," 10.07.2024 - 11.07.2024 "))),a.createElement("td",null,a.createElement("p",null,"Organised and facilitated workshop, titled ",a.createElement("a",{href:"https://www.museumfuernaturkunde.berlin/en/science/virtual-access-fossil-and-archival-material-german-tendaguru-expedition-1909-1913"},"Thinking Through Tendaguru Dinosaurs"),", with Tanzanian Scholars in order to discuss highliting colonial influences in museum data"))),a.createElement("tr",null,a.createElement("th",null,a.createElement("p",null,a.createElement("b",null," 28.02.2024 "))),a.createElement("td",null,a.createElement("p",null,"Talk during the event series 'Kolloquium Provenienzforschung' on the topic ",a.createElement("a",{href:"https://kulturgutverluste.de/en/eventdocumentation/power-and-agency-data-infrastructures-museum-and-archival-collections-colloquium"},"Power and Agency in Data Infrastructures of Museum and Archival Collections")," at German Lost Art Foundation"))))))},5226:function(e,t,n){n.r(t),n.d(t,{Head:function(){return m}});var a=n(6540),l=n(9445),r=n(2568),i=n(4792),o=n(3651),c=n(1693),s=n(4496);t.default=()=>a.createElement(l.A,null,a.createElement(u,null,a.createElement(i.A,null),a.createElement("hr",null),a.createElement("p",null,"Jump to: ",a.createElement(c.Link,{to:"/research#xp"},"Past Research Experience|"),a.createElement(c.Link,{to:"/research#pub"},"Publications|"),a.createElement(c.Link,{to:"/research#t&l"},"Talks & Lectures|"),a.createElement(c.Link,{to:"/research#workshops"},"Workshops|"),a.createElement(c.Link,{to:"/research#edu"},"Education|"),a.createElement(c.Link,{to:"/research#lang"},"Languages|")),a.createElement("hr",null),a.createElement("span",null,a.createElement("h1",null,"Research interests"),a.createElement("p",null,"Digital cultural heritage, Data Feminism, Decolonial intersectional feminism, Data visualisation, Data gaps and biases.")),a.createElement("hr",null),a.createElement("h1",null,"Currently"),a.createElement("p",null,a.createElement("b",null,"Wissenschaftlicher Mitarbeiter (Research Associate)"),a.createElement("br",null)," Since October 2023 ",a.createElement("br",null),"Museum für Naturkunde, Berlin",a.createElement("br",null),"Berlin, Germany"),a.createElement("p",null,a.createElement("b",null,"PhD student "),a.createElement("br",null)," Since April 2023 ",a.createElement("br",null),"Chair of Humanities Data Science and Methodology, TU Darmstadt",a.createElement("br",null),"Darmstadt, Germany"),a.createElement("hr",null),a.createElement("h1",{id:"xp"},"Past Research Experience"),a.createElement("p",null,a.createElement("b",null,"Researcher "),a.createElement("br",null)," August 2022 - April 2023 ",a.createElement("br",null),"Linköping University",a.createElement("br",null),"Norrköping, Sweden"),a.createElement("p",null,a.createElement("b",null,"Research Associate "),a.createElement("br",null)," May 2020 - May 2022 ",a.createElement("br",null),"Urban Complexity Lab, Fachhochschule Potsdam",a.createElement("br",null),"Potsdam, Germany"),a.createElement("p",null,a.createElement("b",null,"Student Research Assistant "),a.createElement("br",null)," May 2019 - April 2020 ",a.createElement("br",null),"Competence Centre Cultural Heritage Digitization, Fraunhofer Institute for Computer Graphics Research IGD",a.createElement("br",null),"Darmstadt, Germany"),a.createElement("p",null,a.createElement("b",null,"Student Assistant "),a.createElement("br",null)," January 2017 - March 2019 ",a.createElement("br",null),"European Centre for Information and Communication Technologies (EICT) GmbH",a.createElement("br",null),"Berlin, Germany"),a.createElement("hr",null),a.createElement("h1",{id:"pub"},"Publications"),a.createElement("p",null,"Díez Díaz, V., Akhlaq, S., Kaiser, K. et al. (2025). Nat. Rev. Biodivers. 1, 145–146 (2025)."," ",a.createElement("a",{href:"https://www.nature.com/articles/s44358-025-00031-2"}," ","Digitization as a research methodology in colonial natural history collections.")," ","https://doi.org/10.1038/s44358-025-00031-2"),a.createElement("p",null,"Depraetere, M., Akhlaq, S., Díaz, V.D., Heumann, I., Schwarz, D. (2025)."," ",a.createElement("a",{href:"https://link.springer.com/chapter/10.1007/978-3-031-78590-0_8#citeas"},"Virtual Access to Fossil & Archival Material from the German Tendaguru Expedition (1909–1913): More Than 100 years of Data-Meta-paradata Management for Improved Standardisation.")," ","In: Ioannides, M., Baker, D., Agapiou, A., Siegkas, P. (eds) 3D Research Challenges in Cultural Heritage V. Lecture Notes in Computer Science, vol 15190. Springer, Cham. https://doi.org/10.1007/978-3-031-78590-0_8"),a.createElement("p",null,"Sara Akhlaq, Arran Ridley, Mark-Jan Bludau and Marian Dörk."," ",a.createElement("a",{href:"https://zenodo.org/record/6594779"},"Data Montage: Towards Coherence in Multimodal Data Representation. DH Benelux 2022: RE-MIX.")," ","Creation and alteration in DH, June 2022."),a.createElement("p",null,"Akhlaq, Sara, de Günther, Sabine, Kreiseler, Sarah, & Dörk, Marian. (2021, December 21).",a.createElement("a",{href:"https://doi.org/10.5281/zenodo.5850850"},"Let's visit museum collections: What can we gather about the data?. vDHd 2021 Experimente (vDHd), virtuell.")),a.createElement("p",null,"Franco, F., Akhlaq, S., Mitchell, B., & Searleman, J. (September, 2021)",a.createElement("a",{href:"https://doi.org/10.1145/3450617.3471248"},"Sisters of Code. Special Interest Group on Computer Graphics and Interactive Techniques Conference.")),a.createElement("p",null,"Sara Akhlaq & Clara Rellensmann. (August, 2018).",a.createElement("a",{href:"https://www.academia.edu/79824172/Heritage_conservation_and_ideologies_a_reader"},"From Church to Mosque to Museum, Influence of Religious Ideologies on Hagia Sophia.")," ","Heritage conservation and ideologies - a reader."),a.createElement("p",null,"Sara Akhlaq. (November, 2019).",a.createElement("a",{href:"https://www.sacredfootsteps.org/2019/11/15/the-other-heritage-hindu-temples-of-pakistan/"},"The ‘Other’ Heritage: Hindu Temples of Pakistan.")," ","Sacred Footsteps."),a.createElement("hr",null),a.createElement("h1",{id:"t&l"},"Talks & Lectures"),a.createElement("p",null,a.createElement("b",null,"Workshop: ")," Organised and facilitated workshop, titled ",a.createElement("a",{href:"https://www.museumfuernaturkunde.berlin/en/science/virtual-access-fossil-and-archival-material-german-tendaguru-expedition-1909-1913"},"Thinking Through Tendaguru Dinosaurs"),", with Tanzanian Scholars in order to discuss highliting colonial influences in museum data"),a.createElement("p",null,a.createElement("b",null,"Talk: ")," During the event series 'Kolloquium Provenienzforschung' on the topic ",a.createElement("a",{href:"https://kulturgutverluste.de/en/eventdocumentation/power-and-agency-data-infrastructures-museum-and-archival-collections-colloquium"},"Power and Agency in Data Infrastructures of Museum and Archival Collections")," at German Lost Art Foundation"),a.createElement("p",null,a.createElement("b",null,"Lecture: ")," During ",a.createElement("a",{href:"https://hdsm.hypotheses.org/1739"},"Advanced Seminar Modern History/History of Technology")," at the division Humanities Data Science and Technology at TU Darmstadt"),a.createElement("p",null,a.createElement("b",null,"Keynote: ")," Titled 'Intersectional Feminist and Anti-colonial Perspectives on Museum Data' at ",a.createElement("a",{href:"https://forge23.uni-tuebingen.de/"},"Forschungsdaten in den Geisteswissenschaften (FORGE) 2023"),"."),a.createElement("p",null," ",a.createElement("b",null,"Panel: ")," Digitalisierung kulturellen Erbes und postkoloniale Perspektiven during ",a.createElement("a",{href:"https://dhd2023.dig-hum.de/page/programmubersich"},"DhD 2023"),"."),a.createElement("p",null," ",a.createElement("b",null,"Conference Talk:")," ‘Data Montage: Towards Coherence in Multimodal Data Representation’ at  ",a.createElement("a",{href:"https://2022.dhbenelux.org/"},"DH Benelux 2022"),"."),a.createElement("p",null,a.createElement("b",null,"Conference Talk: "),"Epistemic Disobedience through Counterdata Visualisations of Digital Cultural Heritage’ at  ",a.createElement("a",{href:"https://dhunbound2022.ach.org/"},"DHUnbound 2022"),"."),a.createElement("p",null,a.createElement("b",null,"Conference Talk: ")," ‘Decolonising Digital Cultural Heritage with Counterdata Visualizations’ at  ",a.createElement("a",{href:" https://vis4dh.dbvis.de/2021/provocations/"},"VIS4DH Workshop"),"."),a.createElement("p",null,a.createElement("b",null,"Panel: ")," ‘Women in Data Visualisation’ in the panel Sister of Code during  ",a.createElement("a",{href:" https://s2021.siggraph.org/presenter/?uid=33877059233152786721"},"SIGGRAPH 2021"),"."),a.createElement("p",null,a.createElement("b",null,"Lecture: ")," Coherent Data Visualisation during the project week at Fachhochschule Potsdam for the Zoophonie course."),a.createElement("p",null,a.createElement("b",null,"Lecture: ")," Practicing Data Feminism: Data biases and Data Ethics during  ",a.createElement("a",{href:" https://uclab.fh-potsdam.de/visoa/"},"Visualizing Open Access Summer School 2021"),"."),a.createElement("hr",null),a.createElement("h1",{id:"workshops"},"Workshops"),a.createElement("p",null,a.createElement("b",null,"Workshop (29.11.2023): ")," Workshop on building bridges between data visualisation and VR/3D experiences at the event  ",a.createElement("a",{href:"https://www.virtuelle-lebenswelten.de/blog-post/how-to-represent-the-virtual-29-11-2023"},"How to Represent the Virtual?")," at the division SFB 1567 Virtuelle Lebenswelten at Ruhr-Universität Bochum"),a.createElement("p",null,a.createElement("b",null,"Workshop (05.12.2022): ")," Carried out a workshop focusing on exploring hidden labor in the open source software project ",a.createElement("a",{href:"https://www.openspaceproject.com/"},"OpenSpace"),"."),a.createElement("p",null,a.createElement("b",null,"Workshop: ")," Workshop on making Data Visualisation Coherent during the project week at Fachhochschule Potsdam for the Zoophonie course."),a.createElement("p",null," ",a.createElement("b",null,"Workshop: ")," ‘Let's visit museum collections! Chapter II’ during  ",a.createElement("a",{href:"https://vdhd2021.hypotheses.org"},"Datenkulturen-Kolloquium 2021"),"."),a.createElement("p",null,a.createElement("b",null,"Workshop: ")," ‘Let's visit museum collections! What can we gather about the data?’ during  ",a.createElement("a",{href:" https://www.uni-potsdam.de/de/digital-humanities/kulturdaten-datenkulturen/10-6-lets-visit-museum-collections"},"vDHd2021"),"."),a.createElement("hr",null),a.createElement("h1",{id:"edu"},"Education"),a.createElement("p",null,a.createElement("b",null,"MA Heritage Conservation and Site Management "),a.createElement("br",null)," October 2016 - October 2019 ",a.createElement("br",null),"Brandenburg University of Technology Cottbus-Senftenberg",a.createElement("br",null),"Cottbus, Germany"),a.createElement("p",null,a.createElement("b",null,"B.Sc. Architectural Engineering "),a.createElement("br",null)," November 2010 - August 2014 ",a.createElement("br",null),"University of Engineering and Technology Lahore",a.createElement("br",null),"Lahore, Pakistan"),a.createElement("hr",null),a.createElement("h1",{id:"lang"},"Languages"),a.createElement("p",null,"Urdu (native level), English (fluent), German (advanced level)."),a.createElement("hr",null)));const u=r.Ay.section`
+  background-color: ${o.h4};
+  padding-top: 30px;
+  padding-bottom: 50px;
+
+  @media only screen and (min-width: 701px) {
+    padding-left: 100px;
+    padding-right: 100px;
+  }
+  @media only screen and (max-width: 700px) {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+`,m=()=>a.createElement(s.k,{title:"Research | Sara Akhlaq"})}}]);
+//# sourceMappingURL=component---src-pages-research-js-ef69c73d40431eb7d47f.js.map
