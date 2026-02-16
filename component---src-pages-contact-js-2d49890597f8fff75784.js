049c610c872773acf090ab9ade06ef796da18c9e
Sara <77059785+alltheakhlaq@users.noreply.github.com>
2026-02-16 13:17:51 +0000

    Updates

---
+++ component---src-pages-contact-js-2d49890597f8fff75784.js
@@ -0,0 +1,22 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[209],{5351:function(e,t,n){n.r(t),n.d(t,{Head:function(){return p}});var a=n(6540),l=n(9445),i=n(2568),o=n(4496),r=(n(8756),n(3651));const d=i.Ay.section`
+  /* background-color: ${r.Rt};
+  padding: 80px; */
+
+  & p {
+    background: ${r.BLACK};
+  }
+
+  @media only screen and (min-width: 701px) {
+    /* padding-left: 100px;
+    padding-right: 100px; */
+    padding-top: 30px;
+    margin-left: 100px;
+    margin-right: 100px;
+  }
+  @media only screen and (max-width: 700px) {
+    padding-top: 10px;
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+`;t.default=()=>a.createElement(l.A,null,a.createElement(d,null,a.createElement("h2",null,"I'm happy to talk more with you!"),a.createElement("hr",null),a.createElement("p",null,"I have a wide variety of interests including but not limited to academic research on application of intersectional feminist principles to different topic in digital humanities. I also offer design and development skills in webdesign, graphic design and front-end web development."),a.createElement("p",null,"If you feel we could work together or if you need any kind of support I can offer, please feel free to get in touch!"),a.createElement("b",null,a.createElement("p",null,"Email: saara.akhlaq@gmail.com")),a.createElement("p",null,"Especially, if you are a woman of color, I am particularly keen to support you in whatever way I can so don't hesitate contacting me!")));const p=()=>a.createElement(o.k,{title:"Contact | Sara Akhlaq"})}}]);
+//# sourceMappingURL=component---src-pages-contact-js-2d49890597f8fff75784.js.map
